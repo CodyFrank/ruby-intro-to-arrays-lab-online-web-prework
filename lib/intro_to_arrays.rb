c7d6130f80b787704625e198cea6ff0840093641
Cody Frank <cody.frank30@gmail.com>
2019-03-16 16:58:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -20,6 +20,6 @@ def last_element(array)
   array[-1]
 end
 
-def first_element_with_array_methods(array, element)
-  array.take(element)
+def first_element_with_array_methods(array)
+  array.shift
 end
